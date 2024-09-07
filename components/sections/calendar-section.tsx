d348bcf396ffc2bf3827728a5588468ee9623d6a
Hugo DEMENEZ <hdemenez@hotmail.fr>
2024-09-07 17:10:57 +0200

    fix: missing import

--- components/sections/calendar-section.tsx
+++ components/sections/calendar-section.tsx
@@ -2,7 +2,6 @@
 import React from 'react'
 import { DateRange } from 'react-day-picker'
 import { CalendarData } from '@/lib/types'
-import CalendarPnl from '../calendar-pnl'
 import NewCalendarPnl from '../calendar/calendar-pnl'
 
 interface CalendarSectionProps {
@@ -14,13 +13,6 @@ export function CalendarSection({ dateRange, calendarData }: CalendarSectionProp
   return (
     <section id="calendar" className="mb-10">
       <NewCalendarPnl calendarData={calendarData}></NewCalendarPnl>
-      {/* <CalendarPnl
-        dateRange={{
-          from: dateRange?.from || new Date(),
-          to: dateRange?.to || new Date(),
-        }}
-        calendarData={calendarData}
-      /> */}
     </section>
   )
 }
