33c3bc12135887a307faa853d7764c0369053206
Hugo DEMENEZ <hdemenez@hotmail.fr>
2024-09-09 17:29:02 +0200

    feat: dialog to import when data is empty

--- components/dashboard.tsx
+++ components/dashboard.tsx
@@ -11,6 +11,8 @@ import { CalendarSection } from './sections/calendar-section'
 import StatisticsSection from './sections/statistics-section'
 import CalendarPnl from './calendar/calendar-pnl'
 import { AnalyticsSection } from './sections/analytics-section'
+import { Dialog, DialogContent, DialogHeader, DialogTitle, DialogDescription, DialogFooter } from "@/components/ui/dialog"
+import ImportButton from './import-csv/import-button'
 
 export default function Dashboard() {
   const { trades } = useTrades()
@@ -19,14 +21,34 @@ export default function Dashboard() {
   const statistics = calculateStatistics(formattedTrades);
   const calendarData: CalendarData = formatCalendarData(formattedTrades);
 
+  const [isDialogOpen, setIsDialogOpen] = useState(trades.length === 0)
+
+  useEffect(() => {
+    setIsDialogOpen(trades.length === 0)
+  }, [trades])
 
   return (
-    <div className="flex flex-col lg:flex-row min-h-screen ">
-      <main className="flex-grow py-4 lg:py-6 overflow-x-hidden">
-        <StatisticsSection statistics={statistics}></StatisticsSection>
-        <GraphsSection calendarData={calendarData} />
-        <CalendarSection calendarData={calendarData} />
-      </main>
-    </div>
+    <>
+      <div className={`flex flex-col lg:flex-row min-h-screen ${isDialogOpen ? 'blur-sm' : ''}`}>
+        <main className="flex-grow py-4 lg:py-6 overflow-x-hidden">
+          <StatisticsSection statistics={statistics}></StatisticsSection>
+          <GraphsSection calendarData={calendarData} />
+          <CalendarSection calendarData={calendarData} />
+        </main>
+      </div>
+      <Dialog open={isDialogOpen} onOpenChange={setIsDialogOpen}>
+        <DialogContent>
+          <DialogHeader>
+            <DialogTitle>No Trades Available</DialogTitle>
+            <DialogDescription>
+              There are currently no trades to display. Please add some trades to see the dashboard content.
+            </DialogDescription>
+          </DialogHeader>
+          <DialogFooter>
+            <ImportButton />
+          </DialogFooter>
+        </DialogContent>
+      </Dialog>
+    </>
   );
 }
