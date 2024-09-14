afe610c6a2991ffc099cd6ad3dd8890b75d86614
Hugo DEMENEZ <hdemenez@hotmail.fr>
2024-09-14 23:33:44 +0200

    fix: stripe issue

--- server/stripe.ts
+++ server/stripe.ts
@@ -5,8 +5,4 @@ import Stripe from "stripe";
 export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string, {
   // https://github.com/stripe/stripe-node#configuration
   apiVersion: "2024-06-20",
-  appInfo: {
-    name: "nextjs-with-stripe-typescript-demo",
-    url: "https://nextjs-with-stripe-typescript-demo.vercel.app",
-  },
 });
