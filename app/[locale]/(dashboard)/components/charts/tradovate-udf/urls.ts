c9a8f2d34524986cc202d062883e06762c93fb6b
Hugo DEMENEZ <hdemenez@hotmail.fr>
2025-04-28 11:14:51 +0200

    refactor(charts): remove deprecated market chart and Tradovate components
    - Delete MarketChart and MarketDataDialog components to streamline the charts directory.
    - Remove associated Tradovate UDF components, including datafeed, services, and socket management.
    - Clean up unused utility functions and styles related to the removed components, enhancing code maintainability.

--- app/[locale]/(dashboard)/components/charts/tradovate-udf/urls.ts
+++
@@ -1,9 +0,0 @@
-export const URLs = {
-    DEMO_URL:       "https://demo.tradovateapi.com/v1",
-    LIVE_URL:       'https://live.tradovateapi.com/v1',
-    MD_URL:         'wss://md.tradovateapi.com/v1/websocket',
-    MD_DEMO_URL:    'wss://md-demo.tradovateapi.com/v1/websocket',
-    WS_DEMO_URL:    'wss://demo.tradovateapi.com/v1/websocket',
-    WS_LIVE_URL:    'wss://live.tradovateapi.com/v1/websocket',
-    CHART_DATA_URL: (r = Math.random()) => `wss://md-demo.tradovateapi.com/v1/websocket?r=${r}`
-}
--- app/[locale]/(dashboard)/components/charts/tradovate-udf/utils/wait-for-ms.ts
+++
@@ -1,7 +0,0 @@
-export const waitForMs = (t: number): Promise<void> => {
-    return new Promise<void>((resolve) => {
-        setTimeout(() => {
-            resolve()
-        }, t)
-    })
-}
