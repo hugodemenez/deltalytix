744578a3ef5cd266a3e595dc80b56fc50dcbcee5
Hugo DEMENEZ <hdemenez@hotmail.fr>
2025-06-04 20:49:16 +0200

    fix(auth): update environment variable retrieval for website URL
    - Changed the method of retrieving the website URL to use process.env.VERCEL_URL instead of NEXT_PUBLIC_VERCEL_URL, ensuring correct URL configuration in production environments.

--- server/auth.ts
+++ server/auth.ts
@@ -7,8 +7,7 @@ import { prisma } from '@/lib/prisma'
 
 export async function getWebsiteURL(){
   let url =
-    process?.env?.NEXT_PUBLIC_SITE_URL ?? // Set this to your site URL in production env.
-    process?.env?.NEXT_PUBLIC_VERCEL_URL ?? // Automatically set by Vercel.
+    process?.env?.VERCEL_URL ?? // Automatically set by Vercel.
     'http://localhost:3000/'
   // Make sure to include `https://` when not localhost.
   url = url.startsWith('http') ? url : `https://${url}`
